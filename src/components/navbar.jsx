909fe31951af57f5fa40a6a9df34e5a46a92d450
JANNAT <rameezbanka@gmail.com>
2024-03-26 16:55:42 +0530

    changes made

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,64 +1,102 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import "./navbar.css";
 import LoginModal from "../pages/login/Login";
 import RegisterModal from "../pages/register/Register";
-
+import SearchIcon from '@mui/icons-material/Search';
 
 export const Navbar = () => {
-   
-const [isLoginModalOpen,setisLoginModalOpen] = useState(false);
-const [isRegistrationModalOpen,setisRegistrationModalOpen] = useState(false);
-const [isLoggedIn, setLoggedIn] = useState(false);
+    const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+    const [isRegistrationModalOpen, setIsRegistrationModalOpen] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // State for tracking login status
+    const [searchQuery, setSearchQuery] = useState("");
+    const [userName, setUserName] = useState(""); // State for storing user's name
 
-const loginHandler = ()=>{   
-    setisLoginModalOpen(true);
-    setisRegistrationModalOpen(false);
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                (isLoginModalOpen || isRegistrationModalOpen) &&
+                !event.target.closest(".header-modal-content") &&
+                !event.target.classList.contains("login")
+            ) {
+                setIsLoginModalOpen(false);
+                setIsRegistrationModalOpen(false);
+            }
+        };
 
-}
-const registrationHandler = ()=>{
-    setisLoginModalOpen(false);
-    setisRegistrationModalOpen(true);
-}
-const modalCloseHandler = ()=>{
-    setisLoginModalOpen(false);
-    setisRegistrationModalOpen(false);
-} 
-const handleLogOut = ()=>{
-    setLoggedIn(false);
-}
+        document.addEventListener("click", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, [isLoginModalOpen, isRegistrationModalOpen]);
+
+    const loginHandler = () => {
+        setIsLoginModalOpen(true);
+        setIsRegistrationModalOpen(false);
+    };
+
+    const registrationHandler = () => {
+        setIsLoginModalOpen(false);
+        setIsRegistrationModalOpen(true);
+    };
+
+    const modalCloseHandler = () => {
+        setIsLoginModalOpen(false);
+        setIsRegistrationModalOpen(false);
+    };
+
+    const handleLogOut = () => {
+        setIsLoggedIn(false); // Update isLoggedIn state to false on logout
+    };
+
+    const handleSearchChange = (e) => {
+        setSearchQuery(e.target.value);
+    };
 
     return (
         <div className="navbar">
-             <div className="links left">
+            <div className="links left">
                 <Link to="/"> Shop </Link>
             </div>
-            <div className="links right">
-            <button className="login" onClick={loginHandler} >Login</button>
-                <Link to="/cart">
-                    <AddShoppingCartIcon />
-                </Link>   
+            <div className="search-bar">
+                <input type="text" placeholder="Search" value={searchQuery} onChange={handleSearchChange}></input>
+                <SearchIcon />
             </div>
-            {(isLoginModalOpen || isRegistrationModalOpen)&&(
-                <div  className="header-modal">
-                    <div className= "header-modal-content">
-                        <div  className="header-auth-container">  
-                        <div  className="header-auth-options">
-                            {isLoggedIn ?(
-                                 <button className="button2" onClick={handleLogOut}>Logout</button>
-                            ):(
-                            <button className="button2"onClick={loginHandler}>Login</button>
-                            )}
-                            <button className="button2"onClick={registrationHandler}>Register</button>
+            <div className="links right">
+                {/* Conditionally render login/logout button based on isLoggedIn state */}
+                {isLoggedIn ? (
+                    <button className="login" onClick={handleLogOut}>Logout</button>
+                ) : (
+                    <button className="login" onClick={loginHandler}>Login</button>
+                )}
+                {isLoggedIn  && (
+                    <Link to="/cart">
+                    <AddShoppingCartIcon />
+                </Link>
+                )}
+            </div>
+            {(isLoginModalOpen || isRegistrationModalOpen) && (
+                <div className="header-modal">
+                    <div className="header-modal-content">
+                        <div className="header-auth-container">
+                            <div className="header-auth-options">
+                                {isLoggedIn ? (
+                                    <div><span>Welcome, {userName}</span>
+                                    <button className="button2" onClick={handleLogOut}>Logout</button>
+                                    </div>
+                                ) : (
+                                    <button className="button2" onClick={loginHandler}>Login</button>
+                                )}
+                                <button className="button2" onClick={registrationHandler}>Register</button>
+                            </div>
                         </div>
-                        </div>
-                        {isLoginModalOpen && <LoginModal onClose={modalCloseHandler} onSuccess ={()=>{setLoggedIn(true)}}/>}
+                        {isLoginModalOpen && <LoginModal onClose={modalCloseHandler} onSuccess={() => setIsLoggedIn(true)} />}
                         {isRegistrationModalOpen && <RegisterModal onClose={modalCloseHandler} />}
                     </div>
                 </div>
             )}
         </div>
-        
-    )
+    );
 };
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,50 +1,61 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import "./login.css";
- const Login = ({onClose, onSuccess }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
-    const handleLogin = async () => {
-        try {
-            const response = await fetch("http://localhost:8080/api/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept":"application/json"
-                },
-                body: JSON.stringify({
-                    email,
-                    password
-                })
-            });
-            const data = await response.json();
-            console.log("Response data:", data);
-            if(response.ok){
-                onClose();
-                onSuccess();
-            }
-        } catch (error) {
-            console.error("Error logging in:", error);
-        }
+const Login = ({ onClose, onSuccess }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          email,
+          password
+        })
+      });
+      const data = await response.json();
+      console.log("Response data:", data);
+      if (response.ok) {
+        onClose();
+        onSuccess();
+      }
+    } catch (error) {
+      console.error("Error logging in:", error);
     }
+  }
 
-
-    return (
-        <div className="login-container-5">
-            <div className="input-container-5">
-                <label className="input-label-5">Email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" 
-                placeholder="Youremail@email.com" className="input-field-5"></input>
-            </div>
-            <div className="input-container-5">
-                <label className="input-label-5" >Password</label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" 
-                placeholder="**********" className="input-field-5"></input>  
-            </div>
-            <div  className="login-button-container-5" >
-                <button className="login-button2" onClick={handleLogin}>Login</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="login-container">
+      <div className="input-container">
+        <label className="input-label">Email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Your email@example.com"
+          className="input-field"
+        />
+      </div>
+      <div className="input-container">
+        <label className="input-label">Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="**********"
+          className="input-field"
+        />
+      </div>
+      <div className="login-button-container">
+        <button className="login-button" onClick={handleLogin}>Login</button>
+      </div>
+    </div>
+  );
 };
+
 export default Login;
